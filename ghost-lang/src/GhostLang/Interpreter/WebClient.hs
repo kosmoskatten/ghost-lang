f08ba8d92050ffd7bbde59bc37fcca0a6308595f
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-08-12 16:15:45 +0200

    Partly implemented pacedConsumer.

--- ghost-lang/src/GhostLang/Interpreter/WebClient.hs
+++ ghost-lang/src/GhostLang/Interpreter/WebClient.hs
@@ -4,9 +4,22 @@ module GhostLang.Interpreter.WebClient
     ) where
 
 import Data.ByteString (ByteString)
+import Data.Time ( NominalDiffTime
+                 , UTCTime
+                 , diffUTCTime
+                 , getCurrentTime
+                 )
+import GHC.Int (Int64)
 import Network.HTTP.Client
 import Network.HTTP.Types
 import qualified Data.ByteString as BS
+
+data VirtualBuffer =
+    VirtualBuffer { totalRec    :: !Int
+                  , bufferPace  :: !Int64
+                  , bufferStart :: !UTCTime
+                  }
+    deriving Show
 
 -- | Fetch a web resource using the provided consumer.
 httpGet :: Manager -> String -> (Response BodyReader -> IO Status) -> IO Status
@@ -30,3 +43,36 @@ eagerConsumer resp = do
                        bs' <- brRead body
                        putStrLn "Got chunk"
                        consumeIt body bs'
+
+{-pacedConsumer :: VirtualBuffer-> Response BodyReader -> IO Status
+pacedConsumer buffer resp = do
+  let status = responseStatus resp
+      body   = responseBody resp-}
+
+-- | Initialize the virtual buffer the pace, expressed as the number
+-- of bytes that are allowed to pass per second (statistically).
+initVirtualBuffer :: Int64 -> IO VirtualBuffer
+initVirtualBuffer pace = do
+  start <- getCurrentTime
+  return $! VirtualBuffer { totalRec    = 0
+                          , bufferPace  = pace
+                          , bufferStart = start
+                          }
+
+addData :: Int -> VirtualBuffer -> VirtualBuffer
+addData amount buffer = buffer { totalRec = amount + totalRec buffer }
+
+isFull :: VirtualBuffer -> IO Bool
+isFull buffer = do
+  now <- getCurrentTime
+  let duration = now `diffUTCTime` bufferStart buffer
+      tp       = totalRec buffer `throughput` duration
+  putStrLn $ show tp
+  return $ tp > bufferPace buffer
+
+throughput :: Int -> NominalDiffTime-> Int64
+throughput amount duration =
+    let duration' = realToFrac duration :: Double
+        rate      = fromIntegral amount / duration'
+    in round rate
+  
